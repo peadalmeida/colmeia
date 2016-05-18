bde01edac51a3e861573aa0b2555fa6f3208a961
pedroaugusto <peadalmeida@gmail.com>
2016-05-17 21:27:19 -0300

    first commit

---
+++ Colmeia/urls.py
@@ -0,0 +1,63 @@
+"""
+Definition of urls for Colmeia.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'app.views.index', name='index'),
+    url(r'^inicio', 'app.views.inicio', name='inicio'),
+    url(r'^search', 'app.views.search', name='search'),
+    url(r'^quemSomos', 'app.views.quemSomos', name='quemSomos'),
+    
+    url(r'^admin', 'app.views.admin', name='admin'),    
+    url(r'^prestadores', 'app.views.prestadores', name='prestadores'),
+    url(r'^contratantes', 'app.views.contratantes', name='contratantes'),
+    url(r'^relatorioAdm', 'app.views.relatorioAdm', name='relatorioAdm'),
+    #Menu Prestador
+    url(r'^cadPrestador', 'app.views.cadPrestador', name='cadPrestador'),    
+    url(r'^servPrestados', 'app.views.servPrestados', name='servPrestados'),
+    url(r'^colmeiaPontos', 'app.views.colmeiaPontos', name='colmeiaPontos'),
+    #Menu Prestador
+    url(r'^cadContratante', 'app.views.cadContratante', name='cadContratante'),    
+    url(r'^servContratados', 'app.views.servContratados', name='servContratados'),
+    url(r'^relContratante', 'app.views.relContratante', name='relContratante'),
+
+    #Site Principal
+    url(r'^s_contato', 'app.views.s_contato', name='s_contato'),
+    url(r'^s_quemSomos', 'app.views.s_quemSomos', name='s_quemSomos'),
+
+    url(r'^sair', 'app.views.index', name='sair'),
+    url(r'^login',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/geral/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Colmeia Login',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ app/views.py
@@ -0,0 +1,254 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+
+#PAGINA INICIAL DO SITE (GERAL)
+def index(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/site/indexSite.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Início',
+            'year':datetime.now().year,
+        })
+    )
+
+#PAGINA INICIAL DO SISTEMA
+def inicio(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Index',
+            'year':datetime.now().year,
+        })
+    )
+
+
+#PAGINA INICIAL DO SISTEMA
+def search(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/geral/resultados.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Resultados',
+            'year':datetime.now().year,
+        })
+    )
+
+
+def s_quemSomos(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/site/s_quemSomos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Quem Somos',
+            'year':datetime.now().year,
+        })
+    )
+
+def s_contato(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/site/s_contato.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Contato',
+            'year':datetime.now().year,
+        })
+    )
+def quemSomos(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/geral/quemSomos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Quem Somos',
+            'year':datetime.now().year,
+        })
+    )
+
+def admin(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/admin/admin.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Administrador',            
+            'year':datetime.now().year,
+        })
+    )
+
+def prestadores(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/admin/prestadores.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Prestadores',            
+            'year':datetime.now().year,
+        })
+    )
+
+def contratantes(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/admin/contratantes.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Contratantes',            
+            'year':datetime.now().year,
+        })
+    )
+
+def relatorioAdm(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/admin/relatorioAdm.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Relatorios',            
+            'year':datetime.now().year,
+        })
+    )
+
+#VIEWS PRESTADORES
+def cadPrestador(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/prestador/frm_prestador.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Cad.Prestador',
+            'year':datetime.now().year,
+        })
+    )
+
+def servPrestados(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/prestador/servPrestados.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Serv.Prestados',
+            'year':datetime.now().year,
+        })
+    )
+
+def colmeiaPontos(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/prestador/colmeiaPontos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | ColPontos',
+            'year':datetime.now().year,
+        })
+    )
+
+
+
+
+#VIEWS CONTRATANTES
+def cadContratante(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contratante/frm_contratante.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Cad. Contratante',
+            'year':datetime.now().year,
+        })
+    )
+
+
+def servContratados(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contratante/frm_contratante.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Serv.Contratados',
+            'year':datetime.now().year,
+        })
+    )
+
+def relContratante(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contratante/relatorioContratante.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Relatorios Contratante',
+            'year':datetime.now().year,
+        })
+    )
+def sair(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Inicio',
+            'year':datetime.now().year,
+        })
+    )
+
+
+
+def login(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/geral/login.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Colmeia | Login',
+            'year':datetime.now().year,
+        })
+    )
