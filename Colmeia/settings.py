dcf57518b34093d4f9b892521b0bc44e18f2fb0a
pedroaugusto <peadalmeida@gmail.com>
2016-05-21 11:13:43 -0300

    behave and splinter deleted

--- Colmeia/settings.py
+++ Colmeia/settings.py
@@ -131,7 +131,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'behave_django',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
